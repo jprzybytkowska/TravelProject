711dd2a1aad66ac82be9e5a30400a02f7dbd2758
tester <jprzybytkowska1@gmail.com>
2024-12-03 13:52:01 +0100

    Search a city

---
+++ TravelProjects/src/test/java/HotelSearch.java
@@ -0,0 +1,30 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class HotelSearch {
+
+
+    @Test
+    public void searchHotel() {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+
+
+
+
+
+    }
+}
